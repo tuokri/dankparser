664d5fe367bc88cb187c4e4f4432f3101924b874
tuokri <tkriikk@tuta.io>
2016-11-14 12:25:30 +0200

    dank updates

--- dank_parser.py
+++ dank_parser.py
@@ -1,67 +1,83 @@
 """Docstring."""
 
-import os
+import re
 import sys
+import urllib.request
 from html.parser import HTMLParser
 
 
 class TwitchQuoteParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
-        self.data_parsed = []
+        self.data = []
+        self.recording = 0
+        # quote_clipboard_copy_content_X
+        self.pattern = re.compile("quote_clipboard_copy_content_\.*")
 
     def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag :", tag)
+        if tag != "span":
+            return
+        if self.recording:
+            self.recording += 1
+            return
+        for name, value in attrs:
+            if name == "id" and self.pattern.match(value):
+                break
+        else:
+            return
+        self.recording = 1
 
     def handle_endtag(self, tag):
-        print("Encountered an end tag  :", tag)
+        if tag == "span" and self.recording:
+            self.recording -= 1
 
     def handle_data(self, data):
-        print("Encountered some data. Memefying.")
-        self.data_parsed.append(data)
-
-    def get_data_parsed(self):
-        return self.data_parsed
-
-
-def read_raw(filename):
-    try:
-        with open(filename, "r", encoding="utf8") as infile:
-            return infile.read().replace("\n", "")
-    except (IOError, FileNotFoundError):
-        print("Dankness overload! Cannot read raw data from file!")
+        if self.recording:
+            self.data.append(data)
 
 
 def write_results(filename, results):
     try:
-        with open(filename, "a", encoding="utf8") as outfile:
+        with open(filename, "w", encoding="utf8") as outfile:
             for r in results:
                 outfile.write(r)
     except (IOError, FileNotFoundError):
         print("Dankness overload! Cannot write results to file!")
 
 
-def scan_directory():
-    filenames = [
-        name for name in os.listdir(".") if name.endswith(".html")]
-    return filenames
+def strip_nlcr(string):
+    string = string.replace("\n", "")
+    string = string.replace("\t", "")
+    return string
+
+
+def get_twitchquotes_urls():
+    urls = []
+    base = "http://www.twitchquotes.com/copypastas?page=XXX&popular=true"
+    for i in range(1, 114):
+        rep = "XXX"
+        urls.append(base.replace(rep, str(i)))
+    return urls
 
 
 def main():
     parser = TwitchQuoteParser()
-    output = "dankness.txt"
+    outfile = "dankness.txt"
 
-    print("Scanning directory for HTML files.")
-    raw_files = scan_directory()
-    print("Scan complete.\n")
+    print("Haxoring twitchquotes.com")
+    urls = get_twitchquotes_urls()
+    print("Done. Beware of the FBI.\n")
 
-    print("Parsing HTML files.")
-    for file_ in raw_files:
-        parser.feed(read_raw(file_))
-    print("Parsing completed.\n")
+    print("Parsing HTML.")
+    for url in urls:
+        print("{}".format(url))
+        parser.feed(
+            strip_nlcr(urllib.request.urlopen(url).read().decode("utf-8")))
+    print("Parsing done.\n")
 
-    print("Writing results to file '{}'.".format(output))
-    write_results(output, parser.get_data_parsed())
+    print("Writing results to file '{}'.".format(outfile))
+    write_results(outfile, parser.data)
+    print("Done.\n")
 
 if __name__ == '__main__':
     try:
